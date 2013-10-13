3e1aa89f1e63b6953467ce9463fb3c791e7b00ee
obsidianart <obsidianart@gmail.com>
2013-10-13 16:34:37 +0100

    moving to require.js

--- js/main.js
+++ js/main.js
@@ -1,69 +1,45 @@
-
-function init(data) {
-	//location, temperature, description, summary
-	var template = $('#weather-template').html();
-	var genTemplate = _.template(template);
-	$(".content-wrapper").html(genTemplate ({
-		location: "London",
-		summary : data.currently.summary,
-		temperature : data.currently.temperature,
-		description : weatherQuotes.partlyCloudy[0]
-	}));
-}
-
-/*
-$.ajax({
-	url: "https://api.forecast.io/forecast//51.5072,0.1275?units=ca",
-	//cache: false,
-	dataType: "json"
-}).done(function( data ) {
-
-
-	var item = data.query.results.channel.item;
-	var i;
-	var foreItem;
-	var foreText;
-	var forecast = "";
-
-	$('#title').html(item.title);
-	$('#today .condition').html(item.condition.text + " and the temperature is " + item.condition.temp + "F");
-
-	for (i=0; i<item.forecast.length; i++) {
-		foreItem = item.forecast[i];
-		foreText = "On" + foreItem.date + " it will be <strong>" + foreItem.text + "</strong> and the temperature is between " + foreItem.low + "F" + " and " + foreItem.high + "F";
-		forecast += '<div><img src="../img/forecast-'+foreItem.code+'.png" alt="'+foreItem.text+'">'+ foreText + '</div>';
+requirejs.config({
+	baseUrl: '',
+	paths: {
+	    text: 'js/vendor/text',
+	    jquery: 'js/vendor/jquery-1.10.1',
+	    underscore: 'js/vendor/underscore-min',
+	    weatherApi: 'https://api.forecast.io/forecast/f3a549e99fe815ba1da83dbe4d5146cb/51.5072,0.1275?units=ca&callback=define'
+	},
+	shim: {
+		'underscore': {
+			exports: '_'
+		}
 	}
-	$('#forecast').html(forecast);
-	
-
-	var forecastTemplate = _.template("On <%= date %> it will be <strong> <%= forecast %></strong> and the temperature is between <%= min %>F and <%= max %>F");
-	for (i=0; i<item.forecast.length; i++) {
-		foreItem = item.forecast[i];
-
-		foreText = forecastTemplate({
-			date:foreItem.date,
-			forecast: foreItem.text,
-			min: foreItem.low,
-			max: foreItem.high
-		});
-		
-		forecast += '<div><img src="../img/forecast-'+foreItem.code+'.png" alt="'+foreItem.text+'">'+ foreText + '</div>';
-	}
-	$('#forecast').html(forecast);
-	
-
-	var template = $('#weather-template').html();
-	var genTemplate = _.template(template);
-
-	$("body").append(genTemplate ({
-		title: "London",
-		condition : "Rainy",
-		forecast : forecast
-	}));
 });
 
-*/
+require([
+	"jquery",
+	"underscore",
+	"weatherApi",
+	"js/quotes",
+	"text!templates/nav.html",
+	"text!templates/main.html"
+	], function (
+		$,
+		_,
+		weather,
+		weatherQuotes,
+		navTemplate,
+		mainTemplate
+	) {
+		var navTemplate = _.template(navTemplate);
+		var mainTemplate = _.template(mainTemplate);
 
-var weatherQuotes = {
-	"partlyCloudy" : ['Just enough <span class="highlighted">sun</span> to forget your <span class="highlighted">umbrella</span>']
-}
+		$('nav').html(navTemplate ({
+
+		}));
+
+		$('#main-forecast').html(mainTemplate ({
+			location: "London",
+			summary : weather.currently.summary,
+			temperature : weather.currently.temperature,
+			description : weatherQuotes.partlyCloudy[0]
+		}));
+	}
+);
---
+++ js/quotes.js
@@ -0,0 +1,9 @@
+define([],
+    function() {
+        return {
+			"partlyCloudy" : [
+				'Just enough <span class="highlighted">sun</span> to forget your <span class="highlighted">umbrella</span>'
+			]
+		}
+    }
+);
